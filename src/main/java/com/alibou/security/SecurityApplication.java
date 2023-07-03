21f19bd99c942b12e265e9f6261c633a9e8ca50d
IVDAL2NIGHT <danielfecalderon07@gmail.com>
2023-07-02 19:45:50 -0500

    código actualizado

--- src/main/java/com/alibou/security/SecurityApplication.java
+++ src/main/java/com/alibou/security/SecurityApplication.java
@@ -2,8 +2,11 @@ package com.alibou.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+
 public class SecurityApplication {
 
 	public static void main(String[] args) {
