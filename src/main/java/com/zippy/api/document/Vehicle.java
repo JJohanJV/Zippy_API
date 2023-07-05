d5f12395d84e433865620f881746559540d6cfe9
Johan Jerez <jerson810@hotmail.com>
2023-07-05 17:17:04 -0500

    cambios en el documento vehiculo

--- src/main/java/com/zippy/api/document/Vehicle.java
+++ src/main/java/com/zippy/api/document/Vehicle.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 import java.util.List;
 
-@Document(collection = "vehicle")
+@Document
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -29,15 +29,5 @@ public class Vehicle {
     private int battery;
     private List<Maintenance> maintenances;
 
-    public Vehicle(String serial, String gps_serial, String model, String type, String status, int kilometers, Date start_up_date, boolean is_electric) {
-        this.serial = serial;
-        this.gps_serial = gps_serial;
-        this.model = model;
-        this.type = type;
-        this.status = status;
-        Kilometers = kilometers;
-        this.start_up_date = start_up_date;
-        this.is_electric = is_electric;
-    }
 }
 
